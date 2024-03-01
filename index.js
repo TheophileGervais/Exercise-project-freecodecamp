d3de2dca0e024958b8fee4071ec61cb01452534b
TheophileGervais <theophile.c.gervais@gmail.com>
2024-03-01 16:00:32 +0100

    commited before pc shutdown because low bat

--- index.js
+++ index.js
@@ -9,9 +9,16 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+let users = [];
 
+app.post("/api/users", (req, res) => {
+  const username = req.body;
 
+  // Generate userID
+  const userID = users.length + 1;
 
+  //Create new user object
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
